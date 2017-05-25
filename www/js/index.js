88dc9b172b5f65ecbf69ed22fe4fe693911276c5
benjamipk <benjaminpapciak@students.berkeley.net>
2017-05-25 10:23:05 -0700

    a

--- www/js/index.js
+++ www/js/index.js
@@ -20,9 +20,43 @@ var app = {
     }
 };
 
-function Confirm(id) {
-    $.post(url, { "studentID": <student_id:string>})
+function confirm(studentid) {
+    if (isNan(studentid) == false) {
+        $.post(url, { "studentID": <student_id:string>})
+
+    }
+    console.log("no");
 }
+
+
+
 app.initialize();
 
 
+
+
+
+
+
+
+$(document).ready(function() {
+
+//dummy test data - get this from an API
+    var y =
+        [{"text": 'Go to Index', "url": '#page1'},
+            {"text": 'Go to Page 2', "url": '#page2'},
+            {"text": 'Go to Page 3', "url": '#page3'},
+            {"text": 'Go to Page 4', "url": '#page4'},
+            {"text": 'Go to Page 5', "url": '#page5'}];
+
+    for(var i=0; i< y.length; i++) {
+
+        $("#thing1,#thing2,#thing3,#thing4,#thing5").append("<li><a  data-transition='pop' href='" + y[i].url + "'>" + y[i].text + "</a></li>")
+
+
+    }
+
+    $("#thing1,#thing2,#thing3,#thing4,#thing5").listview( "refresh" );
+
+
+});
