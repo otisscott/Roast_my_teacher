75254c12689b1b053d40ea8421b85342c5e6dac4
otisscott <otismscott@gmail.com>
2017-05-26 10:30:18 -0700

    Added a colored tab in multitask

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-headercolor.HeaderColor",
+        "file": "plugins/cordova-plugin-headercolor/www/HeaderColor.js",
+        "pluginId": "cordova-plugin-headercolor",
+        "clobbers": [
+            "cordova.plugins.headerColor"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-headercolor": "1.0"
+};
+// BOTTOM OF METADATA
+});
