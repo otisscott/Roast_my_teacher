9b6231826faf4c312baced29ff9c3a32576bf538
paulogle <paugle09@gmail>
2017-05-30 09:44:53 -0700

    Merge branch 'master' of /Users/h205p2/WebstormProjects/Roast_my_teacher with conflicts.

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,62 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
 
-var app_url = "example.com";
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
 
 function confirm(studentid) {
-    for (i=0;i<studentid.length;i++) {
-        if (isNan(studentid[i]) == false) {
-            // $.post(app_url + "/api/login", {"student_id": studentid}, function (data) {
-            //     access_token = data["access_token"];
-            //     console.log(access_token);
-            // });
-            access_token = "gkjdrhso";
-            console.log(access_token);
-        }
+    if (isNan(studentid) == false) {
+        $.post(url, { "studentID": <student_id:string>})
+
     }
-    window.location.replace("error.html");
+    console.log("no");
 }
+
+
+
 app.initialize();
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+//dummy test data - get this from an API
+    var y =
+        [{"text": 'Go to Index', "url": '#page1'},
+            {"text": 'Go to Page 2', "url": '#page2'},
+            {"text": 'Go to Page 3', "url": '#page3'},
+            {"text": 'Go to Page 4', "url": '#page4'},
+            {"text": 'Go to Page 5', "url": '#page5'}];
+
+    for(var i=0; i< y.length; i++) {
+
+        $("#thing1,#thing2,#thing3,#thing4,#thing5").append("<li><a  data-transition='pop' href='" + y[i].url + "'>" + y[i].text + "</a></li>")
+
+
+    }
+.
+    $("#thing1,#thing2,#thing3,#thing4,#thing5").listview( "refresh" );
+
+
+});
