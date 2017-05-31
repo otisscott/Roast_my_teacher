cce0bac24ebed30cc59cb5beba4d4a81b2a0b74e
benjamipk <benjaminpapciak@students.berkeley.net>
2017-05-31 10:17:11 -0700

    a

--- www/js/index.js
+++ www/js/index.js
@@ -5,15 +5,10 @@ var app_url = "example.com";
 function confirm() {
     var studentid = document.getElementById("idinput").value;
     console.log(studentid);
-    for (i=0;i<studentid.length;i++) {
-        if (isNaN(studentid[i]) == false) {
-            window.location.replace("teacherlist.html");
-            access_token = "gkjdrhso";
-            console.log(access_token);
-        }
-        window.location.replace("error.html");
+    if((document.getElementById("idinput").value) == 4) {
+        window.location.replace("teacherlist.html");
     }
-
+    window.location.replace("error.html");
 }
 
 app.initialize();
@@ -22,3 +17,12 @@ app.initialize();
 //     access_token = data["access_token"];
 //     console.log(access_token);
 // });
+
+//for (i=0;i<studentid.length;i++) {
+//if (isNaN(studentid[i]) == false) {
+  //  window.location.replace("teacherlist.html");
+    // access_token = "gkjdrhso";
+  //  console.log(access_token);
+//}
+//window.location.replace("error.html");
+//}
