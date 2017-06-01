879bd385c48a3dece16844928f0f86703db1298f
benjamipk <benjaminpapciak@students.berkeley.net>
2017-06-01 10:29:36 -0700

    a

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,25 @@
 var app_url = "example.com";
+
+
 function confirm() {
     var studentid = document.getElementById("idinput").value;
     console.log(studentid);
-    if((document.getElementById("idinput").value) == 4) {
+    if((document.getElementById("idinput").value) == 4) { //test
         window.location.replace("teacherlist.html");
     }
     window.location.replace("error.html");
 }
 app.initialize();
-// $.post(app_url + "/api/login", {"student_id": studentid}, function (data) {
-//     access_token = data["access_token"];
-//     console.log(access_token);
-// });
+
+
+
+
+
 
 //for (i=0;i<studentid.length;i++) {
 //if (isNaN(studentid[i]) == false) {
   //  window.location.replace("teacherlist.html");
-    // access_token = "gkjdrhso";
-  //  console.log(access_token);
+
 //}
 //window.location.replace("error.html");
 //}
--- www/js/teachers.js
+++ www/js/teachers.js
@@ -2,8 +2,15 @@
  * Created by h205admin on 5/30/17.
  */
 $.getJSON('0.0.0.0:4000/api/teachers', {}, function (data) {
-
+    for(i=0;i<data.length;i++) {
+        teacherlist.push(data[i]);
+        console.log(teacherlist);
+    }
 });
+
+
+
+
 var teacherlist = [];
 //for (var j=0; j < data.length; j++) {
    // teacherlist +
@@ -14,6 +21,9 @@ function appendTeachers() {
         $('#table').append("<li><a href='" + teacherlist[i] + ".html'></a></li>")
     }
 }
+
+
+
 function willisatard() {
     $.post("http://0.0.0.0:4000/api/teachers", {"id": 69, "name": "ALBINSON","subject" : "NOTHING"}, function(){}, 'json')
 }
