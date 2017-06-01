03169fa46b38b468a3bf7a8b81ce7d8d0d8dd182
otisscott <otismscott@gmail.com>
2017-06-01 09:57:26 -0700

    ADDED TEACHER SHIT

---
+++ www/js/teachers.js
@@ -0,0 +1,19 @@
+/**
+ * Created by h205admin on 5/30/17.
+ */
+$.getJSON('0.0.0.0:4000/api/teachers', {}, function (data) {
+
+});
+var teacherlist = [];
+//for (var j=0; j < data.length; j++) {
+   // teacherlist +
+//}
+function appendTeachers() {
+    console.log(data.id);
+    for (var i=0; i < teacherlist.length; i++) {
+        $('#table').append("<li><a href='" + teacherlist[i] + ".html'></a></li>")
+    }
+}
+function willisatard() {
+    $.post("http://0.0.0.0:4000/api/teachers", {"id": 69, "name": "ALBINSON","subject" : "NOTHING"}, function(){}, 'json')
+}
