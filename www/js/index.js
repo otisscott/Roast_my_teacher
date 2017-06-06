a7b438057d6a0d28a164ed7275b9ce32ea1562c5
otisscott <otismscott@gmail.com>
2017-06-06 10:34:05 -0700

    Began styling teachers page, backend integrated

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,13 @@
-var app_url = "http://0.0.0.0:4000";
+var app_url = "http://ec2-54-183-163-136.us-west-1.compute.amazonaws.com";
+var access_token;
+var fullPath;
 function confirm() {
     var studentID = $(".idcheck").val();
     console.log(studentID);
-    //if((studentID) == 4) {
-    //    window.location.replace("teachers.html");
-    //}
-    //window.location.replace("error.html");
     $.post(app_url + "/api/login", {"student_id": studentID.toString()}, function (data) {
         access_token = data["access_token"];
         console.log(data);
-        window.location = "/teachers.html?access_token=" + access_token;
+        window.location.href = "/teachers.html?access_token=" + access_token;
     }).fail(function (data) {
         console.log(data)
     });
