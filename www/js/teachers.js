12a66bbbc944c3da6e9cd4e0bd6de4aceca688e4
otisscott <otismscott@gmail.com>
2017-06-09 10:20:26 -0700

    jQuery Mobile is gone

--- www/js/teachers.js
+++ www/js/teachers.js
@@ -7,29 +7,7 @@ function appendTeachers() {
         var teachers = data["data"];
         console.log(teachers);
         for (var i = 0; i < teachers.length; i++) {
-
-            $('#table').append("<li><a href='teacherprofile.html?id=" + teachers[i].id + "'>"+ teachers[i].name + "</a></li>")
+            $('#table').append("<li class='list-group-item'><a href='teacherprofile.html?id=" + teachers[i].id + "'>"+ teachers[i].name + "</a></li>")
         }
-
     });
-}
-
-
-function teacher1() {
-    window.location.replace("teacherprofile.html");
-}
-
-function css(el) {
-    var sheets = document.styleSheets, ret = [];
-    el.matches = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector
-        || el.msMatchesSelector || el.oMatchesSelector;
-    for (var i in sheets) {
-        var rules = sheets[i].rules || sheets[i].cssRules;
-        for (var r in rules) {
-            if (el.matches(rules[r].selectorText)) {
-                ret.push(rules[r].cssText);
-            }
-        }
-    }
-    return ret;
 }
