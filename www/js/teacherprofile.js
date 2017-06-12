8912855e4adf12700798e8ae78e378ad10a50520
paulogle <paugle09@gmail>
2017-06-12 10:49:17 -0700

    teacher profile javascript

--- www/js/teacherprofile.js
+++ www/js/teacherprofile.js
@@ -4,24 +4,31 @@
 var baseUrl = "http://ec2-54-183-163-136.us-west-1.compute.amazonaws.com";
 
 //get the url, get the id from url, add id to /api/teachers/:id/roast
-window.onload = function() {
+/**window.onload = function() {
     var urlParams = new URLSearchParams(window.location.search);
     var id = urlParams.get('id');
     noFun(id)
-};
-/**$(window).ready(function () {
+};**/
+var loadData = function() {
     var urlParams = new URLSearchParams(window.location.search);
     var id = urlParams.get('id');
     noFun(id)
-});**/
+}
 
 function noFun(id) {
     $.getJSON(baseUrl + "/api/teachers/" + id + "/roasts", function (data) {
-        var roasts = data.data
+        var roasts = data.data;
         for(i=0; i < roasts.length ; i++) {
-            comment = roasts[i].comment
-            rating = roasts[i].rating
+            comment = roasts[i].comment;
+            rating = roasts[i].rating;
             $("#roasts").append("<li>" + rating + ":" + comment + "</li>")
         }
+        if(roasts.length == 0){
+            $("#roasts").append("No roasts, get roasting!")
+        }
     })
+}
+
+function newroast(id) {
+
 }
